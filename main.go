7091c6099d4737f7059f95183672dbbc523940b0
mpl <mathieu.lonjaret@gmail.com>
2011-12-20 16:25:57 +0100

    c-c-c combo breaker

--- main.go
+++ main.go
@@ -34,19 +34,19 @@ func main() {
 
 	// insert input
 	for k,v := range input {
-		err = c.Insert(&bson.M{k:v})
+		err = c.Insert(bson.M{k:v})
 		if err != nil {
 			log.Fatal("insert: " + err.Error())
 		}
 	}
 
 	// update an existing
-	_, err = c.Upsert(bson.M{"three": bson.M{"$exists":true}}, &bson.M{"eight":"8"})
+	_, err = c.Upsert(bson.M{"three": bson.M{"$exists":true}}, bson.M{"eight":"8"})
 	if err != nil {
 		log.Fatal("update: " + err.Error())
 	}
 	// update a non existing
-	_, err = c.Upsert(bson.M{"foo": bson.M{"$exists":true}}, &bson.M{"seven":"7"})
+	_, err = c.Upsert(bson.M{"foo": bson.M{"$exists":true}}, bson.M{"seven":"7"})
 	if err != nil {
 		log.Fatal("update: " + err.Error())
 	}
@@ -60,7 +60,7 @@ func main() {
 	// insert escaped dot
 	illegal := "foo.bar"
 	encoded := base64.StdEncoding.EncodeToString([]byte(illegal))
-	err = c.Insert(&bson.M{encoded:"fooball"})
+	err = c.Insert(bson.M{encoded:"fooball"})
 	if err != nil {
 		log.Fatal("insert: " + err.Error())
 	}
@@ -86,4 +86,60 @@ func main() {
 	if err != nil {
 		log.Fatal("get: " + err.Error())
 	}
+
+	input2 := []string{
+		"7",
+		"8",
+		"9",
+		"10",
+		"11",
+	}
+
+	// insert input
+	for _,v := range input2 {
+		encoded = base64.StdEncoding.EncodeToString([]byte("foobee"))
+		err = c.Insert(bson.M{encoded:v})
+		if err != nil {
+			log.Fatal("insert: " + err.Error())
+		}
+	}
+
+	// iter foobees
+	result = nil
+	encoded = base64.StdEncoding.EncodeToString([]byte("foobee"))
+	it = c.Find(bson.M{encoded:&bson.M{"$exists": true}}).Iter()
+	for it.Next(&result) {
+		fmt.Println("****foobees****")
+		for k,v := range result {
+			fmt.Println(k, v)
+		}
+		result = nil
+	}
+	if it.Err() != nil {
+		log.Fatal(it.Err())
+	}
+
+	// insert similar values
+	err = c.Insert(bson.M{"hello":"world"})
+	if err != nil {
+		log.Fatal("insert: " + err.Error())
+	}
+	err = c.Insert(bson.M{"hello":"war"})
+	if err != nil {
+		log.Fatal("insert: " + err.Error())
+	}
+	// test regexp
+	result = nil
+	it = c.Find(bson.M{"hello": bson.M{"$regex": "^w"}}).Iter()
+	for it.Next(&result) {
+		fmt.Println("****hellos****")
+		for k,v := range result {
+			fmt.Println(k, v)
+		}
+		result = nil
+	}
+	if it.Err() != nil {
+		log.Fatal(it.Err())
+	}
+
 }
