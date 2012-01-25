ea97d296909261905b2ec2bc3079f154d0fd1edb
mpl <mathieu.lonjaret@gmail.com>
2012-01-25 17:13:33 +0100

    short var declarations

--- pidstatAvg.go
+++ pidstatAvg.go
@@ -21,33 +21,30 @@ func main() {
 		log.Fatal(err)
 	}
 
-	var line string
-	var fields []string
-	var cpu, cum float64
-	count := 0
+	sum, count := float64(0), 0
 	b := bufio.NewReader(f)
 	for i:=0; i<3; i++ {
 		b.ReadString('\n')
 	}
 	for {
-		line, err = b.ReadString('\n')
+		line, err := b.ReadString('\n')
 		if err != nil {
 			if err == io.EOF {
 				break
 			}
 			log.Fatal(err)
 		}
-		fields = strings.Fields(line)
+		fields := strings.Fields(line)
 		if len(fields) != 9 {
 			log.Fatal("incorrect nb of fields in line")
 		}
-		cpu, err = strconv.ParseFloat(fields[6], 32)
+		cpu, err := strconv.ParseFloat(fields[6], 32)
 		if err != nil {
 			log.Fatal(err)
 		}
-		cum += cpu
+		sum += cpu
 		count++
 	}
-	cpu = cum / float64(count)
+	cpu := sum / float64(count)
 	log.Printf("%g \n", cpu)
 }
