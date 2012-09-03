22a385e968e021639f0aac42db57fc2f6a39e7a5
mpl <mathieu.lonjaret@gmail.com>
2012-09-04 00:16:38 +0200

    it should not serve anymore on :7070. shit.

--- selfLearn.go
+++ selfLearn.go
@@ -2,30 +2,51 @@ package main
 
 import (
 	"fmt"
-//	"html"
 	"log"
 	"net/http"
+	"net"
+	"time"
+	"sync"
 )
 
+var once sync.Once
 func learnSelfHost(addr string) (c chan string) {
 	c = make(chan string, 1)
-	go func () {
-		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			discoHost := r.Host
-			fmt.Fprintf(w, "Hello, %q", discoHost)
-			c <- discoHost
-			close(c)
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		panic(err)
+	}
+	learner := http.NewServeMux()
+	learner.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		once.Do(func(){
+		lis.Close()
+		discoHost := r.Host
+		c <- discoHost
+		println("host sent")
+		c <- discoHost
+		close(c)
+		http.Redirect(w, r, "http://localhost:9090", http.StatusFound)
 		})
-		if err := http.ListenAndServe(addr, nil); err == nil {
+	})
+	go func () {
+		if err := http.Serve(lis, learner); err == nil {
 			log.Fatalf("Problem during learning phase: %v", err)
 		}
+		println("listener closed")
 	}()
 	return c
 }
 
 func main() {
-	c := learnSelfHost("0.0.0.0:9090")
+	c := learnSelfHost("0.0.0.0:7070")
 	host := <- c
 	println(host)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, you")
+	})
+	go http.ListenAndServe("localhost:9090", nil)
+	println("main now serving")
+	<- c
+	time.Sleep(60 * time.Second)
 }
 
