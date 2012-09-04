cbaf0d8f88c24e3b8f82f4eb486fcad820fc3086
mpl <mathieu.lonjaret@gmail.com>
2012-09-04 23:35:26 +0200

    kinda works to close the connection. sometimes.

--- selfLearn.go
+++ selfLearn.go
@@ -10,6 +10,7 @@ import (
 )
 
 var once sync.Once
+
 func learnSelfHost(addr string) (c chan string) {
 	c = make(chan string, 1)
 	lis, err := net.Listen("tcp", addr)
@@ -18,14 +19,26 @@ func learnSelfHost(addr string) (c chan string) {
 	}
 	learner := http.NewServeMux()
 	learner.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		println("been there")
 		once.Do(func(){
-		lis.Close()
-		discoHost := r.Host
-		c <- discoHost
-		println("host sent")
-		c <- discoHost
-		close(c)
-		http.Redirect(w, r, "http://localhost:9090", http.StatusFound)
+			println("done that")
+			lis.Close()
+			discoHost := r.Host
+			c <- discoHost
+			println("host sent")
+			c <- discoHost
+			close(c)
+			w.Header().Set("Connection", "close")
+			hj, ok := w.(http.Hijacker)
+			if !ok {
+				log.Fatalf("no hj")
+			}
+			conn, _, err := hj.Hijack()
+			if err != nil {
+				log.Fatal(err)
+			}
+			conn.Close()
+//			http.Redirect(w, r, "http://localhost:9090", http.StatusFound)
 		})
 	})
 	go func () {
@@ -38,13 +51,17 @@ func learnSelfHost(addr string) (c chan string) {
 }
 
 func main() {
-	c := learnSelfHost("0.0.0.0:7070")
+	c := learnSelfHost("0.0.0.0:9090")
 	host := <- c
 	println(host)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, you")
 	})
-	go http.ListenAndServe("localhost:9090", nil)
+	go func() {
+		if err := http.ListenAndServe("localhost:9090", nil); err != nil {
+			log.Fatal(err)
+		}
+	}()
 	println("main now serving")
 	<- c
 	time.Sleep(60 * time.Second)
