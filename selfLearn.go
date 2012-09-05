a9fa8c576fd14c4b197525fcc0b8e690c1c1fef4
mpl <mathieu.lonjaret@gmail.com>
2012-09-05 19:07:23 +0200

    wip

--- selfLearn.go
+++ selfLearn.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,24 +11,60 @@ import (
 )
 
 var once sync.Once
+var lis *stoppableListener
+var conns []net.Conn
+var stopped = errors.New("listener stopped")
+
+type signal struct{}
+
+type stoppableListener struct {
+        net.Listener
+        stop chan signal
+}
+
+func (sl *stoppableListener) Accept() (c net.Conn, err error) {
+		// non-blocking read on the stop channel
+		select {
+		default:
+				// nothing
+		case <-sl.stop:
+				for _,c := range conns {
+					c.Close()
+				}
+				return nil, stopped
+		}
+
+		// if we got here, we have not been asked to stop, so call
+		// Accept on the underlying listener.
+
+		c, err = sl.Listener.Accept()
+		if err != nil {
+				return
+		}
+		conns = append(conns, c)
+		return
+}
 
 func learnSelfHost(addr string) (c chan string) {
 	c = make(chan string, 1)
-	lis, err := net.Listen("tcp", addr)
+	var sig signal
+	l, err := net.Listen("tcp", addr)
 	if err != nil {
 		panic(err)
 	}
+	lis = &stoppableListener{Listener: l, stop: make(chan signal, 1)}
 	learner := http.NewServeMux()
 	learner.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		println("been there")
 		once.Do(func(){
+			lis.stop <- sig
 			println("done that")
-			lis.Close()
 			discoHost := r.Host
 			c <- discoHost
 			println("host sent")
 			c <- discoHost
 			close(c)
+/*
 			w.Header().Set("Connection", "close")
 			hj, ok := w.(http.Hijacker)
 			if !ok {
@@ -38,6 +75,7 @@ func learnSelfHost(addr string) (c chan string) {
 				log.Fatal(err)
 			}
 			conn.Close()
+*/
 //			http.Redirect(w, r, "http://localhost:9090", http.StatusFound)
 		})
 	})
@@ -45,7 +83,7 @@ func learnSelfHost(addr string) (c chan string) {
 		if err := http.Serve(lis, learner); err == nil {
 			log.Fatalf("Problem during learning phase: %v", err)
 		}
-		println("listener closed")
+		println("server done")
 	}()
 	return c
 }
