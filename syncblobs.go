089cd2a54b1ad12fccf2f0f2818e60bf8ba43ef7
mpl <mathieu.lonjaret@gmail.com>
2014-03-31 00:45:40 +0200

    now using github.com/mpl/gocron

--- syncblobs.go
+++ syncblobs.go
@@ -6,22 +6,18 @@ import (
 	"log"
 	"os"
 	"os/exec"
-	"net/smtp"
 	"time"
-)
 
-const (
-	alert   = "Subject: syncBlobs alert."
-	interval = time.Hour
+	"github.com/mpl/gocron"
 )
 
 var (
-	emailTo   = flag.String("emailto", "mpl@smgl.fr.eu.org", "address where to send an alert when failing")
 	emailFrom = flag.String("emailfrom", "mpl@oenone", "alert sender email address")
-	smtpAddr  = flag.String("smtp", "serenity:25", "where to relay the message")
+	notiPort  = flag.Int("port", 9687, "port for the local http server used for browser notifications")
+	once      = flag.Bool("once", false, "whether to run this sync only once instead of every hour")
 )
 
-func syncBlobs() {
+func syncBlobs() error {
 	args := []string{"sync", "-src=granivore", "-dest=/home/mpl/var/camlistore-granivore/blobs/"}
 	cmd := exec.Command("/home/mpl/bin/camtool-grani", args...)
 	env := os.Environ()
@@ -30,56 +26,36 @@ func syncBlobs() {
 	// TODO(mpl): make it timeout or something in case of a 401. better yet, capture stderr,
 	// and die if see anything there.
 	cmd.Env = env
-	if err := cmd.Run(); err != nil {
-		msg := fmt.Sprintf("%s\n\n%v", alert, err)
-		err = SendMail(*smtpAddr, *emailFrom, []string{*emailTo}, []byte(msg))
-		if err != nil {
-			// TODO(mpl): show notification :)
-			log.Fatal(err)
-		}
-	}
-}
-
-// SendMail connects to the server at addr, authenticates with the
-// optional mechanism a if possible, and then sends an email from
-// address from, to addresses to, with message msg.
-func SendMail(addr string, from string, to []string, msg []byte) error {
-	c, err := smtp.Dial(addr)
-	if err != nil {
-		return err
-	}
-	defer c.Close()
-	if err = c.Mail(from); err != nil {
-		return err
-	}
-	for _, addr := range to {
-		if err = c.Rcpt(addr); err != nil {
-			return err
-		}
-	}
-	w, err := c.Data()
-	if err != nil {
-		return err
-	}
-	_, err = w.Write(msg)
-	if err != nil {
-		return err
-	}
-	err = w.Close()
-	if err != nil {
-		return err
-	}
-	return c.Quit()
+	return cmd.Run()
 }
 
 func main() {
 	flag.Parse()
-	if *emailTo == "" || *emailFrom == "" {
-		log.Fatal("Need emailfrom and emailto")
+	if *emailFrom == "" {
+		log.Fatal("Need emailfrom")
 	}
-	// TODO(mpl): Y U NO SHOW FROM anymore?
-	for {
-		syncBlobs()
-		time.Sleep(interval)
+
+	interval := time.Hour
+	if *once {
+		interval = 0
 	}
+	cron := gocron.Cron{
+		Interval: interval,
+		Job:      syncBlobs,
+		Mail: &gocron.MailAlert{
+			Subject: "Syncblobs error",
+			To:      []string{"mpl@mpl.fr.eu.org"},
+			From:    *emailFrom,
+			SMTP:    "serenity:25",
+		},
+		Notif: &gocron.Notification{
+			Host: fmt.Sprintf("localhost:%d", *notiPort),
+			Msg:  "Syncblobs error",
+		},
+		File: &gocron.StaticFile{
+			Path: "/home/mpl/var/log/syncblobs.log",
+			Msg:  "gocron error",
+		},
+	}
+	cron.Run()
 }
